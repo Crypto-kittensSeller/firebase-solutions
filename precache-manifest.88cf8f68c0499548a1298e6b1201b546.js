b4a528b1cc4f926586bb6177d5a5ae761a70a728
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2019-07-15 01:34:00 +0300

    Updates

---
+++ precache-manifest.88cf8f68c0499548a1298e6b1201b546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e1f02e8f80bb9df2358b770e07fe1f",
+    "url": "/firebase-solutions/index.html"
+  },
+  {
+    "revision": "5739955a138bd2691038",
+    "url": "/firebase-solutions/static/css/main.17b60f07.chunk.css"
+  },
+  {
+    "revision": "2219237e5186f536ae69",
+    "url": "/firebase-solutions/static/js/2.b866d933.chunk.js"
+  },
+  {
+    "revision": "5739955a138bd2691038",
+    "url": "/firebase-solutions/static/js/main.f4b3da83.chunk.js"
+  },
+  {
+    "revision": "4ebba40271bac186df2a",
+    "url": "/firebase-solutions/static/js/runtime~main.21c0f63d.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/firebase-solutions/static/media/showcase.4b31330b.jpg"
+  }
+]);
